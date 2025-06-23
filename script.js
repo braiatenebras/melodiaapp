9df47bdb9347d8d1063721547a72dd920348d0fe
davidefreitas <davi.conceicao.freitas@escola.pr.gov.br>
2025-06-23 07:57:49 -0300

    melhorando um pouco a landing page

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('visivel');
-        } else {
-          entry.target.classList.remove('visivel');
-        }
-      });
-    }, {
-      threshold: 0.1 // ativa quando 10% do elemento está visível
+document.addEventListener('DOMContentLoaded', () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visivel');
+      }
     });
-
-    const elementos = document.querySelectorAll('.beneficios, .depoimentos');
-    elementos.forEach(el => observer.observe(el));
+  }, {
+    threshold: 0.1 // ativa quando 10% do elemento está visível
   });
+
+  const elementos = document.querySelectorAll('.beneficios, .depoimentos, .rodape, .contato');
+  elementos.forEach(el => observer.observe(el));
+});
